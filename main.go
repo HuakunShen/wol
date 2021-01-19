490525198d612841e94b7ca04a338da812771638
Huakun Shen <shenhuakun@outlook.com>
2021-01-19 09:28:46 -0500

    update main.go argument parser help message

--- main.go
+++ main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
-	wol "github.com/HuakunShen/wol/wol-go"
 	"strconv"
+
+	wol "github.com/HuakunShen/wol/wol-go"
 )
 
 func main() {
 	port := flag.Int("port", 9, "Port Number, Default: 9")
 	ip := flag.String("ip", "255.255.255.255", "ip, Default: 255.255.255.255")
-	mac := flag.String("mac", "", "ip, Default: 255.255.255.255")
+	mac := flag.String("mac", "", "mac address of target machine")
 	flag.Parse()
 	fmt.Println("IP:\t\t", *ip)
 	fmt.Println("Port:\t\t", *port)
