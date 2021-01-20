b7d2236c69d1e4723f6187c415c901a221465af8
Huakun Shen <shenhuakun@outlook.com>
2021-01-20 18:12:12 -0500

    Update help menu and add installation script install.sh

--- main.go
+++ main.go
@@ -11,15 +11,21 @@ import (
 func main() {
 	port := flag.Int("port", 9, "Port Number, Default: 9")
 	ip := flag.String("ip", "255.255.255.255", "ip, Default: 255.255.255.255")
-	mac := flag.String("mac", "", "mac address of target machine")
+	mac := flag.String("mac", "", "mac address of target machine (required)")
 	flag.Parse()
-	fmt.Println("IP:\t\t", *ip)
-	fmt.Println("Port:\t\t", *port)
-	fmt.Println("Mac Address:\t", *mac)
-	err := wol.WakeOnLan(*mac, *ip, strconv.Itoa(*port))
-	if err != nil {
-		fmt.Println("Error:", err)
+	if flag.NArg() == 0 {
+		fmt.Println("No argument received, at least give me a mac address")
+		flag.PrintDefaults()
 	} else {
-		fmt.Println("Success")
+		fmt.Println("IP:\t\t", *ip)
+		fmt.Println("Port:\t\t", *port)
+		fmt.Println("Mac Address:\t", *mac)
+		err := wol.WakeOnLan(*mac, *ip, strconv.Itoa(*port))
+		if err != nil {
+			fmt.Println("Error:", err)
+		} else {
+			fmt.Println("Success")
+		}
 	}
+
 }
