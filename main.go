f9584feb257cbc1bb0e45ad86615711a0a3ddc35
Huakun Shen <huakun.shen@huakunshen.com>
2021-03-12 21:43:04 -0500

    Update argument parser

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	ip := flag.String("ip", "255.255.255.255", "ip, Default: 255.255.255.255")
 	mac := flag.String("mac", "", "mac address of target machine (required)")
 	flag.Parse()
-	if flag.NArg() == 0 {
+	if flag.NFlag() == 0 {
 		fmt.Println("No argument received, at least give me a mac address")
 		flag.PrintDefaults()
 	} else {
