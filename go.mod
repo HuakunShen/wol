f060abad4cefadb02cac95137961b5b1cd4f57ca
Huakun Shen <huakun.shen@huakunshen.com>
2025-01-26 03:12:38 -0500

    feat: cobra

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/HuakunShen/wol
 
 go 1.16
+
+require github.com/spf13/cobra v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,31 +1,42 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
 package main
 
-import (
-	"flag"
-	"fmt"
-	"strconv"
-
-	wol "github.com/HuakunShen/wol/wol-go"
-)
+import "github.com/HuakunShen/wol/cmd"
 
 func main() {
-	port := flag.Int("port", 9, "Port Number, Default: 9")
-	ip := flag.String("ip", "255.255.255.255", "ip, Default: 255.255.255.255")
-	mac := flag.String("mac", "", "mac address of target machine (required)")
-	flag.Parse()
-	if flag.NFlag() == 0 {
-		fmt.Println("No argument received, at least give me a mac address")
-		flag.PrintDefaults()
-	} else {
-		fmt.Println("IP:\t\t", *ip)
-		fmt.Println("Port:\t\t", *port)
-		fmt.Println("Mac Address:\t", *mac)
-		err := wol.WakeOnLan(*mac, *ip, strconv.Itoa(*port))
-		if err != nil {
-			fmt.Println("Error:", err)
-		} else {
-			fmt.Println("Success")
-		}
-	}
-
+	cmd.Execute()
 }
+
+// package main
+
+// import (
+// 	"flag"
+// 	"fmt"
+// 	"strconv"
+
+// 	wol "github.com/HuakunShen/wol/wol-go"
+// )
+
+// func main() {
+// 	port := flag.Int("port", 9, "Port Number, Default: 9")
+// 	ip := flag.String("ip", "255.255.255.255", "ip, Default: 255.255.255.255")
+// 	mac := flag.String("mac", "", "mac address of target machine (required)")
+// 	flag.Parse()
+// 	if flag.NFlag() == 0 {
+// 		fmt.Println("No argument received, at least give me a mac address")
+// 		flag.PrintDefaults()
+// 	} else {
+// 		fmt.Println("IP:\t\t", *ip)
+// 		fmt.Println("Port:\t\t", *port)
+// 		fmt.Println("Mac Address:\t", *mac)
+// 		err := wol.WakeOnLan(*mac, *ip, strconv.Itoa(*port))
+// 		if err != nil {
+// 			fmt.Println("Error:", err)
+// 		} else {
+// 			fmt.Println("Success")
+// 		}
+// 	}
+
+// }
